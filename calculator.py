589571c511d9aa4bda72c82d77ba69d5b9854f7a
David Wood <davidwood1301@gmail.com>
2019-12-08 01:31:39 +0000

    Changed multiply to lambda to reduce parameter parsing
    Should reduce memory usage when applied to larger projects (e.g. using larger integers/for loops for iterative usage)

--- calculator.py
+++ calculator.py
@@ -4,9 +4,11 @@ def add(a,b):
 def subtract(a,b):
     return a-b
 
-def multiply(a,b):
-    return a*b
+def multiply(n):
+    return lambda a : a * n
 
 def divide(a,b):
-    return a/b
+    return a / b
 
+def squared(a):
+    return a*a
--- calculator_GUI.py
+++ calculator_GUI.py
@@ -1,5 +1,9 @@
 import calculator
 
+multiple2 = calculator.multiply(2)
+
 num1 = int(input(">>> "))
 num2 = int(input(">>> "))
 print("Sum of the two numbers:",calculator.add(num1,num2))
+print("Square of first number:",calculator.squared(num1))
+print("first num times 2:",multiple2(num1))
